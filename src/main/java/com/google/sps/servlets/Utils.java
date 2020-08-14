e2cca3b6f5680232382ec1449c0c391e5641d95e
Gábor Fuisz <fuiszg@google.com>
2020-08-14 15:24:22 +0000

    Add oauth authentication

---
+++ src/main/java/com/google/sps/servlets/Utils.java
@@ -0,0 +1,93 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.google.sps.servlets;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Preconditions;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
+class Utils {
+
+  /**
+   * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
+   * globally shared instance across your application.
+   */
+  private static final AppEngineDataStoreFactory DATA_STORE_FACTORY =
+      AppEngineDataStoreFactory.getDefaultInstance();
+  
+  /** Global instance of the HTTP transport. */
+  static final HttpTransport HTTP_TRANSPORT = new UrlFetchTransport();
+
+  /** Global instance of the JSON factory. */
+  static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+  private static GoogleClientSecrets clientSecrets = null;
+
+  static GoogleClientSecrets getClientCredential() throws IOException {
+    if (clientSecrets == null) {
+      clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+          new InputStreamReader(Utils.class.getResourceAsStream("/client_secrets.json")));
+      Preconditions.checkArgument(!clientSecrets.getDetails().getClientId().startsWith("Enter ")
+          && !clientSecrets.getDetails().getClientSecret().startsWith("Enter "),
+          "Download client_secrets.json file from https://code.google.com/apis/console/"
+          + "?api=calendar into calendar-appengine-sample/src/main/resources/client_secrets.json");
+    }
+    return clientSecrets;
+  }
+
+  static String getRedirectUri(HttpServletRequest req) {
+    GenericUrl url = new GenericUrl(req.getRequestURL().toString());
+    url.setRawPath("/oauth2callback");
+    return url.build();
+  }
+
+  static GoogleAuthorizationCodeFlow newFlow() throws IOException {
+    return new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+            getClientCredential(), Collections.singleton(CalendarScopes.CALENDAR_EVENTS)).setDataStoreFactory(
+            DATA_STORE_FACTORY).setAccessType("offline").build();
+  }
+
+  /**
+   * Returns an {@link IOException} (but not a subclass) in order to work around restrictive GWT
+   * serialization policy.
+   */
+  static IOException wrappedIOException(IOException e) {
+    if (e.getClass() == IOException.class) {
+      return e;
+    }
+    return new IOException(e.getMessage());
+  }
+
+  private Utils() {
+  }
+}

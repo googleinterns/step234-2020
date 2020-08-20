0b7565eb418b044369bcaf66b209ad5774a4dffd
Gábor Fuisz <fuiszg@google.com>
2020-08-20 16:32:24 +0000

    Refactor TasksProvider

--- src/main/java/com/google/sps/api/tasks/TasksProvider.java
+++ src/main/java/com/google/sps/api/tasks/TasksProvider.java
@@ -16,6 +16,7 @@ package com.google.sps.api.tasks;
 
 import com.google.api.services.tasks.model.Task;
 import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,19 +25,17 @@ public class TasksProvider {
   public static final ImmutableList<String> TASKS_SAMPLE_TITLES = ImmutableList.of(
       "Review code", "Write the design doc", "Talk to PM", "Investigate report", "Prepare slides");
 
-  private static final Task FIRST = new Task();
-  private static final Task SECOND = new Task();
-  private static final Task ANOTHER = new Task();
-  private static final Task YET_ANOTHER = new Task();
-  private static final Task LAST = new Task();
-  public static final List<Task> TASK_MODEL_LIST = Arrays.asList(FIRST, SECOND, ANOTHER, YET_ANOTHER, LAST);
+
+  public static final List<Task> TASK_MODEL_LIST = new ArrayList<Task>();
 
   public TasksProvider() {
     int index = 0;
-    for (Task task : TASK_MODEL_LIST) {
+    for (String title: TASKS_SAMPLE_TITLES) {
+      Task task = new Task();
       task.setId(Integer.toString(index));
       task.setTitle(TASKS_SAMPLE_TITLES.get(index));
       index++;
+      TASK_MODEL_LIST.add(task);
     }
   }
 
